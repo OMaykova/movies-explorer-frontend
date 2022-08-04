e1a95b2a0ceeb8748730a59f9070de51f9607df2
Olga Maykova <dolkaaaaa@gmail.com>
2022-08-04 17:33:04 +0300

    feat: techs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject'
+import Techs from '../Techs/Techs';
 import './Main.css';
 
 function Main({handlePromoButton}) {
@@ -8,8 +9,8 @@ function Main({handlePromoButton}) {
     <div>
       <Promo handlePromoButton={handlePromoButton}/>
       <AboutProject />
-      {/* <Techs />
-      <AboutMe />
+      <Techs />
+      {/* <AboutMe />
       <Portfolio /> */}
     </div>
   )
