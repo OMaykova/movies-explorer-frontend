ffcdb925afd784f62793add705101656424bff51
Olga Maykova <dolkaaaaa@gmail.com>
2022-08-10 22:40:33 +0300

    feat: movies

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import './Movie.css'
-import movie from '../../images/movie.svg';
-import like from '../../images/like.svg'
+import like from '../../images/like.svg';
 
-function Movie() {
+function Movie({movie}) {
+  function getTimeFromMins(mins, movie) {
+    let hours = Math.trunc(mins/60);
+    let minutes = mins % 60;
+    return hours + 'ч ' + minutes + 'м';
+};
   return(
     <figure className='movie-template'>
-      <img className='movie__image' src={movie} alt='Обложка фильма'/>
+      <img className='movie__image' src={movie.image.url} alt='Обложка фильма'/>
       <figcaption className='movie__caption'>
         <div className='movie__container'>
-          <h3 className='movie__title'>33 слова о дизайне</h3>
+          <h3 className='movie__title'>{movie.nameRU}</h3>
           <button className='movie__like' type='button' title='Нравится' aria-label="Кнопка нравится"><img className='movie__like-image' src={like} alt='Нравится' /></button>
         </div>
-        <p className='movie__duration'>1ч 47м</p>
+        <p className='movie__duration'>{getTimeFromMins(movie.duration)}</p>
       </figcaption>
     </figure>
   )
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import Movie from '../Movie/Movie'
-import './MoviesCardList.css'
+import Movie from '../Movie/Movie';
+import './MoviesCardList.css';
+import movies from '../../utils/movies';
 
 function MoviesCardList() {
   return(
     <section className='movies-card-list'>
-      {/* {
+      <div className='movies-grid'>
+      {
         movies.map(movie => 
-          <Movie />
+          <Movie 
+            key={movie.id}
+            movie={movie}
+          />
         )
-      } */}
-      <Movie />
+      }
+      </div>
       <button className='movies__button-more' type='button' title='Ещё' aria-label='Кнопка ещё'>Ещё</button>
     </section>
   )
