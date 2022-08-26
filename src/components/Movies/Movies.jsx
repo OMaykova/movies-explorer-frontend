53db1be0cbd70b7efbce93ccde89f058c955a2e8
Olga Maykova <dolkaaaaa@gmail.com>
2022-08-26 15:45:49 +0300

    feat: hook resize

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -5,38 +5,7 @@ import Preloader from '../Preloader/Preloader';
 import './Movies.css';
 
 function Movies({movies, isLoading, nothingFound, handlSearchMovie, searchError, setSearchError}) {
-  // const [moviesOfSearch, setMoviesOfSearch] = useState([]);
-  // const [searchValue, setSearchValue] = useState('');
-  // const [checkbox, setCheckbox] = useState(false);
 
-  // useEffect(() => {
-  //   localStorage.setItem('moviesOfSearch', JSON.stringify(moviesOfSearch));
-  // }, [moviesOfSearch, checkbox, searchValue])
-  // useEffect(() => {
-  //   localStorage.setItem('searchValue', JSON.stringify(searchValue));
-  // }, [searchValue])
-  // useEffect(() => {
-  //   localStorage.setItem('checkbox', JSON.stringify(checkbox));
-  // }, [checkbox])
-
-  // function handlSearchMovie(e) {
-  //   setIsLoading(true);
-  //   e.preventDefault();
-  //   const resultOfSearchByValue = movies.filter(movie =>
-  //     isCyrillic(searchValue.toLowerCase()) ?
-  //       movie.nameRU.toLowerCase().includes(searchValue.toLowerCase())
-  //       :
-  //       movie.nameEN.toLowerCase().includes(searchValue.toLowerCase())
-  //   )
-  //   const resultOfSearchByCheck = resultOfSearchByValue.filter(movie => 
-  //     checkbox === true ?
-  //       movie.duration <= 40
-  //       :
-  //       movie
-  //     )
-  //   setMoviesOfSearch(resultOfSearchByCheck);
-  //   setIsLoading(false);
-  // }
   return(
     <section className='movies'>
       <SearchForm 
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import Movie from '../Movie/Movie';
 import './MoviesCardList.css';
-// import movies from '../../utils/movies';
 import savedMovies from '../../utils/savedMovies';
 import { useLocation } from "react-router-dom";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { useEffect } from "react";
+
 
 function MoviesCardList({movies, isLoading, nothingFound, searchError}) {
   const location = useLocation()
   const classNameMoviesList = isLoading || nothingFound || searchError ? 'movies-card-list movies-card-list_hidden' : 'movies-card-list';
+  const screenWidth = useWindowDimensions();
+  // window.screen.width;
+  const [counter, setCounter] = useState({total: screenWidth.width, more: 3})
+    // screenWidth >= 1024 ? 12
+    // :
+    // screenWidth > 480 ? 8
+    // :
+    // 5
+  // )
+
+  const [showMovie, setShowMovie] = useState([]);
+  
+
+  useEffect(() => {
+    if (location.pathname === '/movies') {
+      console.log(screenWidth.width)
+    if (screenWidth.width > 1024) {
+      setCounter({total:12, more: 3})
+    } else if (screenWidth.width < 1024 && screenWidth.width > 480) {
+      setCounter({total:8, more: 2})
+    } else if (screenWidth.width < 480) {
+      setCounter({total:5, more: 2})
+    }
+    console.log(counter)
+    }
+  }, [location.pathname, screenWidth])
+
+  useEffect(() => {
+    if (movies.length) {
+      const list = movies.slice(0, counter.total);
+      setShowMovie(list);
+    }
+  }, [movies, counter.total])
+
+  // function handleMoreButton() {
+  //   screenWidth >= 1024 ? setCounter(counter + 3)
+  //   :
+  //   setCounter(counter + 2)
+  // }
+  function handleMoreButton() {
+    let start = counter.total + counter.more
+    setCounter({total: start, more: counter.more})
+  }
+
 
   return(
     <>
@@ -22,7 +68,7 @@ function MoviesCardList({movies, isLoading, nothingFound, searchError}) {
             <>
               <ul className='movies-grid'>
                 {
-                  movies.map(movie => 
+                  showMovie.map(movie => 
                     <li className='movies-grid__container'>
                       <Movie 
                         key={movie.id}
@@ -32,7 +78,11 @@ function MoviesCardList({movies, isLoading, nothingFound, searchError}) {
                   )
                 }
               </ul>
-              <button className='movies__button-more' type='button' title='Ещё' aria-label='Кнопка ещё'>Ещё</button>
+              {movies.length > 3 && counter.total < movies.length ?
+                <button className='movies__button-more' onClick={handleMoreButton} type='button' title='Ещё' aria-label='Кнопка ещё'>Ещё</button>
+                :
+                ''
+              }
             </>
           }
           {location.pathname === '/saved-movies' &&
