5c24bc6123e23154f3ef25bf7bc21101cb8bdf19
Olga Maykova <dolkaaaaa@gmail.com>
2022-09-01 23:19:15 +0300

    fix links

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,18 +5,18 @@ function Portfolio() {
   return (
     <section className='portfolio'>
       <h3 className='portfolio__title'>Портфолио</h3>
-      <ul className='portfolio__container' href='https://omaykova.github.io/how-to-learn/' target='blank'>
+      <a className='portfolio__container' href='https://omaykova.github.io/how-to-learn/' target='blank'>
         <li className='portfolio__link'>Статичный сайт</li>
         <li className='portfolio__arrow'></li>
-      </ul>
-      <ul className='portfolio__container' href='https://omaykova.github.io/russian-travel/' target='blank'>
+      </a>
+      <a className='portfolio__container' href='https://omaykova.github.io/russian-travel/' target='blank'>
         <li className='portfolio__link'>Адаптивный сайт</li>
         <li className='portfolio__arrow'></li>
-      </ul>
-      <ul className='portfolio__container' href='https://omaykova.nomoredomains.xyz' target='blank'>
+      </a>
+      <a className='portfolio__container' href='https://omaykova.nomoredomains.xyz' target='blank'>
         <li className='portfolio__link'>Одностраничное приложение</li>
         <li className='portfolio__arrow'></li>
-      </ul>
+      </a>
     </section>
   )
 }
